62510efec094c8442e6666903712fa68ac140d97
JOÃO REIS <5754290@formacao.iefp.pt>
2022-03-22 14:58:10 +0000

    Alteracao de nome dos campos

--- Migrations/20220322145151_alteracao de nametype.Designer.cs
+++ Migrations/20220322145151_alteracao de nametype.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Restaurante.Models;
 
 namespace Restaurante.Migrations
 {
     [DbContext(typeof(RestauranteContext))]
-    partial class RestauranteContextModelSnapshot : ModelSnapshot
+    [Migration("20220322145151_alteracao de nametype")]
+    partial class alteracaodenametype
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20220322145151_alteracao de nametype.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Restaurante.Migrations
+{
+    public partial class alteracaodenametype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "varchar(50)",
+                table: "Registo",
+                newName: "Nome");
+
+            migrationBuilder.RenameColumn(
+                name: "varchar(30)",
+                table: "Registo",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "int(9)",
+                table: "Registo",
+                newName: "Telefone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Registo",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Registo",
+                type: "varchar(30)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registo_Email",
+                table: "Registo",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Registo_Email",
+                table: "Registo");
+
+            migrationBuilder.RenameColumn(
+                name: "Telefone",
+                table: "Registo",
+                newName: "int(9)");
+
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Registo",
+                newName: "varchar(50)");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Registo",
+                newName: "varchar(30)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "varchar(50)",
+                table: "Registo",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "varchar(30)",
+                table: "Registo",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(30)");
+        }
+    }
+}
